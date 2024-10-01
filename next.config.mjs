e5311b7d2b1cd0b1c3972f4afea7bafab9d2621e
Skajosborn <135985308+skajosborn@users.noreply.github.com>
2024-10-01 11:19:57 -0400

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: false,
-    Images:{
-        domains:['res.cloudinary.com']
-    }
-}
-
-export default nextConfig;
